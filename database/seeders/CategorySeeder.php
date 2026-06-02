09bf773b2313781dc89df50021e65397c39f3824
IlfanAsykuri <ilfanhs88bp@gmail.com>
2026-06-02 19:11:37 +0700

    Add Income Catagory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,42 +12,97 @@ class CategorySeeder extends Seeder
     {
         $categories = [
             // Pengeluaran
-            ['name' => 'Makanan',     'type' => 'expense', 'is_default' => false,
-             'keywords' => ['makan', 'nasi', 'warteg', 'warung', 'ayam', 'bakso', 'mie', 'soto', 'indomie', 'rendang', 'restoran']],
+            [
+                'name' => 'Makanan',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['makan', 'nasi', 'warteg', 'warung', 'ayam', 'bakso', 'mie', 'soto', 'indomie', 'rendang', 'restoran']
+            ],
 
-            ['name' => 'Minuman',     'type' => 'expense', 'is_default' => false,
-             'keywords' => ['kopi', 'coffee', 'teh', 'minuman', 'jus', 'bubble', 'boba', 'susu', 'es', 'starbucks', 'jahe']],
+            [
+                'name' => 'Minuman',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['kopi', 'coffee', 'teh', 'minuman', 'jus', 'bubble', 'boba', 'susu', 'es', 'starbucks', 'jahe']
+            ],
 
-            ['name' => 'Transport',   'type' => 'expense', 'is_default' => false,
-             'keywords' => ['bensin', 'pertamax', 'solar', 'parkir', 'grab', 'gojek', 'ojek', 'bus', 'kereta', 'toll', 'tol', 'angkot']],
+            [
+                'name' => 'Transport',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['bensin', 'pertamax', 'solar', 'parkir', 'grab', 'gojek', 'ojek', 'bus', 'kereta', 'toll', 'tol', 'angkot']
+            ],
 
-            ['name' => 'Hiburan',     'type' => 'expense', 'is_default' => false,
-             'keywords' => ['game', 'topup', 'netflix', 'spotify', 'bioskop', 'cinema', 'main', 'hiburan', 'steam', 'nonton']],
+            [
+                'name' => 'Hiburan',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['game', 'topup', 'netflix', 'spotify', 'bioskop', 'cinema', 'main', 'hiburan', 'steam', 'nonton']
+            ],
 
-            ['name' => 'Belanja',     'type' => 'expense', 'is_default' => false,
-             'keywords' => ['beli', 'belanja', 'shopee', 'tokopedia', 'lazada', 'indomaret', 'alfamart', 'minimarket', 'baju', 'sepatu', 'tas']],
+            [
+                'name' => 'Belanja',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['beli', 'belanja', 'shopee', 'tokopedia', 'lazada', 'indomaret', 'alfamart', 'minimarket', 'baju', 'sepatu', 'tas']
+            ],
 
-            ['name' => 'Utilitas',    'type' => 'expense', 'is_default' => false,
-             'keywords' => ['listrik', 'pln', 'token', 'air', 'pdam', 'internet', 'wifi', 'pulsa', 'data', 'indihome', 'telkom']],
+            [
+                'name' => 'Utilitas',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['listrik', 'pln', 'token', 'air', 'pdam', 'internet', 'wifi', 'pulsa', 'data', 'indihome', 'telkom']
+            ],
 
-            ['name' => 'Kesehatan',   'type' => 'expense', 'is_default' => false,
-             'keywords' => ['obat', 'dokter', 'apotek', 'klinik', 'vitamin', 'rumah sakit', 'rs', 'puskesmas', 'konsultasi']],
+            [
+                'name' => 'Kesehatan',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['obat', 'dokter', 'apotek', 'klinik', 'vitamin', 'rumah sakit', 'rs', 'puskesmas', 'konsultasi']
+            ],
 
-            ['name' => 'Pendidikan',  'type' => 'expense', 'is_default' => false,
-             'keywords' => ['buku', 'kursus', 'les', 'sekolah', 'kuliah', 'biaya', 'pendidikan', 'jurnal', 'spp', 'tuition']],
+            [
+                'name' => 'Pendidikan',
+                'type' => 'expense',
+                'is_default' => false,
+                'keywords' => ['buku', 'kursus', 'les', 'sekolah', 'kuliah', 'biaya', 'pendidikan', 'jurnal', 'spp', 'tuition']
+            ],
 
-            ['name' => 'Lain-lain',   'type' => 'expense', 'is_default' => true,
-             'keywords' => []],
+            [
+                'name' => 'Lain-lain',
+                'type' => 'expense',
+                'is_default' => true,
+                'keywords' => []
+            ],
 
             // Pemasukan
-            ['name' => 'Gaji',        'type' => 'income', 'is_default' => false,
-             'keywords' => ['gaji', 'salary', 'upah', 'honor', 'honorarium', 'payroll']],
+            [
+                'name' => 'Gaji',
+                'type' => 'income',
+                'is_default' => false,
+                'keywords' => ['gaji', 'salary', 'upah', 'honor', 'honorarium', 'payroll']
+            ],
 
-            ['name' => 'Freelance',   'type' => 'income', 'is_default' => false,
-             'keywords' => ['freelance', 'proyek', 'project', 'klien', 'client', 'jasa']],
+            [
+                'name' => 'Freelance',
+                'type' => 'income',
+                'is_default' => false,
+                'keywords' => ['freelance', 'proyek', 'project', 'klien', 'client', 'jasa']
+            ],
 
-            ['name' => 'Pemasukan Lain', 'type' => 'income', 'is_default' => false,
-             'keywords' => ['cair', 'terima', 'transfer masuk', 'bonus', 'reward', 'cashback', 'refund', 'kembali']],
+            [
+                'name' => 'Kiriman',
+                'type' => 'income',
+                'is_default' => false,
+                'keywords' => ['kiriman', 'kiriman ibu', 'kiriman bulanan']
+            ],
+
+            [
+                'name' => 'Pemasukan Lain',
+                'type' => 'income',
+                'is_default' => false,
+                'keywords' => ['cair', 'terima', 'transfer masuk', 'bonus', 'reward', 'cashback', 'refund', 'kembali']
+            ],
         ];
 
         foreach ($categories as $cat) {
